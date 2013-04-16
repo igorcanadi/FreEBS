3e4b0c658e7e5289ab8e5ceaf03b5f8680ce1f0f
James Paton <jpaton2@gmail.com>
2013-04-15 19:00:48 -0500

    use mmapped file
    Signed-off-by: James Paton <jpaton2@gmail.com>

--- kernel/client.py
+++ kernel/client.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 import socket
 import struct
+import mmap
 
-MEMORY = ['\0'] * 1024 * 1024
+size = 1024 * 1024
 
 host = ''
 port = 9000
@@ -10,22 +13,24 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(backlog)
 
-while True:
-    client, address = s.accept()
+with open('replica.dsk', 'r+b') as f:
+    mm = mmap.mmap(f.fileno(), 0)
     while True:
-        command = client.recv(14)
-        c, l, o, sn = struct.unpack('!HIII', command)
-        print "SEQ %d" % sn
-        if c == 1: # write
-            print "Writing %d bytes at %d offset" % (l, o)
-            data = ''
-            while len(data) < l:
-                data = data + client.recv(l - len(data))
-            for i in range(0, l):
-                MEMORY[o*512 + i] = data[i]
-            client.send(struct.pack('!HI', 0, sn))
-        else: # read
-            print "Reading %d bytes at %d offset" % (l, o)
-            client.send(struct.pack('!HI', 0, sn))
-            client.send(''.join(MEMORY[o*512 : o*512 + l]))
-    client.close()
+        client, address = s.accept()
+        while True:
+            command = client.recv(14)
+            c, l, o, sn = struct.unpack('!HIII', command)
+            #print "SEQ %d" % sn
+            if c == 1: # write
+                #print "Writing %d bytes at %d offset" % (l, o)
+                data = ''
+                while len(data) < l:
+                    data = data + client.recv(l - len(data))
+                for i in range(0, l):
+                    mm[o*512 + i] = data[i]
+                client.send(struct.pack('!HI', 0, sn))
+            else: # read
+                #print "Reading %d bytes at %d offset" % (l, o)
+                client.send(struct.pack('!HI', 0, sn))
+                client.send(''.join(mm[o*512 : o*512 + l]))
+        client.close()
