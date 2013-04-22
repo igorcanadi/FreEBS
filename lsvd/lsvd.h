23e38c19af30dab4648d7656e760197a3f824a1b
Igor Canadi <igor.canadi@gmail.com>
2013-04-22 12:58:21 -0500

    we don't need no fsync

--- lsvd/lsvd.h
+++ lsvd/lsvd.h
@@ -33,5 +33,6 @@ int read_lsvd(struct lsvd_disk *lsvd, void *buf,
         uint64_t length, uint64_t offset, uint64_t version);
 int write_lsvd(struct lsvd_disk *lsvd, const void *buf,
         uint64_t length, uint64_t offset, uint64_t version);
+int fsync_lsvd(struct lsvd_disk *lsvd);
 uint64_t get_version(struct lsvd_disk *lsvd);
 int close_lsvd(struct lsvd_disk *lsvd);
