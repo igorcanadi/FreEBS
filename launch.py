a6601089e0827495b3d060f74731e70894a19da3
James Paton <jpaton2@gmail.com>
2013-05-06 17:15:56 -0500

    seems to work except for client
    Signed-off-by: James Paton <jpaton2@gmail.com>

--- launch.py
+++ launch.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
-import argparse, socket, sh, getpass, paramiko
+import argparse, socket, sh, getpass, paramiko, time, threading, sys
+
+class Piper(threading.Thread):
+    def __init__(self, inp, out, host):
+        self.inp = inp
+        self.out = out
+        self.host = host
+        super(Piper, self).__init__(target=self)
+        self.daemon = True
+
+    def __call__(self, *args, **kwargs):
+        for line in self.inp:
+            self.out.write(str(self.host) + ': ' + line)
 
 class Host(object):
+    hosts = list()
+
     def connect(self, username, password):
         # first do the SSHClient
         self.client = paramiko.SSHClient()
@@ -13,10 +27,13 @@ class Host(object):
         self.transport = paramiko.Transport((self.hostname, 22))
         self.transport.connect(username=username, password=password)
         self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        Host.hosts.append(self)
 
     def do(self, cmd):
         print self.hostname + ': ' + cmd
-        return self.client.exec_command(cmd)
+        (_, out, err) = self.client.exec_command(cmd)
+        Piper(out, sys.stdout, self).start()
+        Piper(err, sys.stderr, self).start()
 
     def copy(self, local, remote):
         rv = self.sftp.put(local, remote)
@@ -30,6 +47,9 @@ class Host(object):
     def __del__(self):
         self.client.close()
         self.transport.close()
+
+    def __str__(self):
+        return self.hostname
 
 def get_pass(username):
     return getpass.getpass('Password for %s@cs: ' % username)
@@ -49,8 +69,8 @@ def main():
             default='./kernel/freebs.ko',
             help='local filename of kernel module')
     parser.add_argument('--rmgr', nargs=1, type=str,
-            default='./storage_daemon/sdaemon',
-            help='local filename of storage daemon')
+            default='~/bin/sdaemon',
+            help='remote filename of storage daemon')
     args = parser.parse_args()
 
     username = args.username
@@ -63,15 +83,24 @@ def main():
     print 'installing sdaemon...'
     for i in range(len(replicas)):
         replica = replicas[i]
-        replica.copy(args.rmgr, '/tmp/sdaemon')
-        replica.do('rm -f /scratch/vdisk')
+        #replica.copy(args.rmgr, '/tmp/sdaemon')
+        replica.do('rm -f /tmp/vdisk')
         if i == 0:
-            replica.do('/tmp/sdaemon -c /scratch/vdisk')
+            replica.do(args.rmgr + ' -c /tmp/vdisk -n ' + replicas[i + 1].hostname)
+        elif i == len(replicas) - 2:
+            replica.do(args.rmgr + ' -c /tmp/vdisk -p ' + replicas[i - 1].hostname + 
+                    ' -n ' + replicas[i + 1].hostname)
         else:
-            replica.do('/tmp/sdaemon -c /scratch/vdisk -p ' + replicas[i - 1].hostname)
+            replica.do(args.rmgr + ' -c /tmp/vdisk -p ' + replicas[i - 1].hostname)
 
     client.copy(args.module, '/tmp/freebs.ko')
-    client.do('sudo insmod /tmp/freebs.ko --replicas ' + ','.join([i.ip for i in replicas]))
+    client.do('sudo insmod /tmp/freebs.ko replicas=' + ','.join([i.ip for i in replicas]))
+
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt, e:
+        pass
 
 if __name__ == '__main__':
     main()
