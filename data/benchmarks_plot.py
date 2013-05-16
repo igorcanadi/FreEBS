208e1965f6f52c2c862c1015e1102809e925ea51
James Paton <paton@cs.wisc.edu>
2013-05-16 01:34:42 -0500

    added latency numbers for kernel build
    Signed-off-by: James Paton <paton@cs.wisc.edu>

--- data/benchmarks_plot.py
+++ data/benchmarks_plot.py
@@ -4,31 +4,35 @@ import matplotlib.pyplot as plt
 FONT_SIZE = 17          
 
 def get_data(filename):
-    lines = open(filename, 'r').read().split('\n')[:-1]
+    lines = open(filename, 'r').readlines()
     data = {}
 
     for l in lines:
         a, b = l.split()
+        print a, b
         data[tuple(a.split('.'))] = float(b)
 
     relative_data = {}
+
+    print '\n'.join(map(str, data.keys())) + '\n'
 
     for d in data:
         if d[1] == 'freebs':
             relative_data['.'.join(d)] = data[d] / data[(d[0], 'local', d[2])]
 
-    xlabs = [("iometer.freebs.read", "IOmeter read"),
-            ("iometer.freebs.write", "IOmeter write"),
-            ("read.freebs.read", "Sequential reads"), 
-            ("write.freebs.write", "Sequential writes"),
-            ("randr.freebs.read", "Random reads"),
-            ("randw.freebs.write", "Random writes"),]
-            #("kernel.freebs.result", "Linux kernel build")]
+    return relative_data
 
-    return relative_data, xlabs
+xlabs = [("iometer.freebs.read", "IOmeter read"),
+    ("iometer.freebs.write", "IOmeter write"),
+    ("read.freebs.read", "Sequential reads"), 
+    ("write.freebs.write", "Sequential writes"),
+    ("randr.freebs.read", "Random reads"),
+    ("randw.freebs.write", "Random writes"),
+    ("kernel.freebs.result", "Linux kernel build")]
 
-relative_throughputs, tlabs = get_data('throughput.dat')
-relative_latencies, llabs = get_data('latency.dat')
+relative_throughputs = get_data('throughput.dat')
+relative_throughputs['kernel.freebs.result'] = 0
+relative_latencies = get_data('latency.dat')
 ind = np.arange(len(relative_throughputs))
 width = 0.2
 fig = plt.figure()
@@ -36,20 +40,20 @@ ax = fig.add_subplot(111)
 ax.tick_params(axis='both', labelsize=FONT_SIZE)
 plt.subplots_adjust(bottom=0.3)
 rects = ax.bar(ind + 0.25, 
-    map(lambda x: relative_throughputs[x[0]], tlabs), 
+    map(lambda x: relative_throughputs[x[0]], xlabs), 
     width, 
     color='b', 
     label='throughput')
 rects = ax.bar(ind + 0.45, 
-    map(lambda x: relative_latencies[x[0]], llabs), 
+    map(lambda x: relative_latencies[x[0]], xlabs), 
     width, 
     color='r', 
     label='latency')
 
 ax.set_ylabel('Relative speedup over local', fontsize=FONT_SIZE)
 ax.set_xticks(ind + 0.45)
-ax.set_xticklabels(map(lambda x: x[1], tlabs), rotation=90, fontsize=FONT_SIZE)
+ax.set_xticklabels(map(lambda x: x[1], xlabs), rotation=90, fontsize=FONT_SIZE)
 ax.legend(loc='upper left')
 
 #plt.show()
-fig.savefig('../paper/figures/throughput.pdf', format='pdf', bbox_inches='tight')
+fig.savefig('../paper/figures/benchmarks.pdf', format='pdf', bbox_inches='tight')
