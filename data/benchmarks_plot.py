beec3d135ecca6b3c94221a93b5886246e9b5266
Igor Canadi <igor.canadi@gmail.com>
2013-05-16 00:15:59 -0500

    evaluation

--- data/benchmarks_plot.py
+++ data/benchmarks_plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 lines = open('throughput.dat', 'r').read().split('\n')[:-1]
 data = {}
 
+FONT_SIZE = 17
 for l in lines:
     a, b = l.split()
     data[tuple(a.split('.'))] = float(b)
@@ -26,11 +27,13 @@ xlabs = [("iometer.freebs.read", "IOmeter read"),
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax.tick_params(axis='both', labelsize=FONT_SIZE)
 plt.subplots_adjust(bottom=0.3)
 rects = ax.bar(ind + 0.25, map(lambda x: relative_data[x[0]], xlabs), width, color='b')
 
+ax.set_ylabel('Relative speedup over local', fontsize=FONT_SIZE)
 ax.set_xticks(ind + 0.25 + width/2)
-ax.set_xticklabels(map(lambda x: x[1], xlabs), rotation=90)
+ax.set_xticklabels(map(lambda x: x[1], xlabs), rotation=90, fontsize=FONT_SIZE)
 
 #plt.show()
-fig.savefig('../paper/figures/benchmarks.pdf', format='pdf')
+fig.savefig('../paper/figures/benchmarks.pdf', format='pdf', bbox_inches='tight')
